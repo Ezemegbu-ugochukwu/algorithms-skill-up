3cdf524d13e72e87707b9e528043c337ea9faf17
Ezemegbu-ugochukwu <expandee50@yahoo.com>
2022-07-31 13:09:36 +0100

    algo practice

---
+++ src/com/ugo/findCharacterLargerThanTargetElement.java
@@ -0,0 +1,24 @@
+package com.ugo;
+
+public class findCharacterLargerThanTargetElement {
+    public static void main(String[] args) {
+     char [] num = {'a', 'c', 'e', 'f', 'g'};
+     char target = 'g';
+        System.out.println(findCharacterLargerThanTargetElement(num, target));
+    }
+    //note arr = ['c', 'e', 'f', 'g']  target = g  ans = c
+    public static char findCharacterLargerThanTargetElement (char [] num, int target) {
+
+        int start = 0;
+        int end = num.length -1;
+        while (start <= end){
+            int mid  = start + (end - start)/2;
+            if (num[mid] > target){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return num[start % num.length];
+    }
+}
