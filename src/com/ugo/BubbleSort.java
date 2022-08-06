91aa9eece406316ec98431ebef59ecfbefc5223b
Ezemegbu-ugochukwu <expandee50@yahoo.com>
2022-08-06 16:42:39 +0100

    algorithm practice

---
+++ src/com/ugo/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.ugo;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int [] num = {3,5,2,1,4,0};
+        System.out.println(Arrays.toString(bubbleSort(num)));
+    }
+     static int[] bubbleSort(int [] arr){
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++){
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++){
+                if (arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped){
+                break;
+            }
+        }
+         return arr;
+     }
+}
