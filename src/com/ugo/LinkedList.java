b434b912d848dca534c9e6d6519340264ae15d77
Ezemegbu-ugochukwu <expandee50@yahoo.com>
2023-05-04 05:23:07 +0100

    current commit

--- src/com/ugo/LinkedList.java
+++ src/com/ugo/LinkedList.java
@@ -2,7 +2,14 @@ package com.ugo;
 
 public class LinkedList {
     public static void main(String[] args) {
+       LinkedList ll = new LinkedList();
+       ll.insertFirst(3);
+       ll.insertFirst(4);
+       ll.insertFirst(1);
+       ll.insertFirst(8);
+       ll.insertLast(99);
 
+       ll.display();
     }
 //    public listNode reverseList (listNode head) {
 //       listNode prev = null;
@@ -22,8 +29,36 @@ public class LinkedList {
         this.size = 0;
     }
 
-    public void insertFirst(int value) {
+    public void insertLast(int value){
+        if (tail == null){
+            insertFirst(value);
+            return;
+        }
+        Node node = new Node(value);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
 
+    public void insertFirst(int value) {
+       Node node = new Node(value);
+       node.next = head;
+       head = node;
+
+       if (tail == null){
+           tail = head;
+       }
+
+       size += 1;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value +"->");
+            temp = temp.next;
+        }
+        System.out.println("End");
     }
 
     private class Node {
